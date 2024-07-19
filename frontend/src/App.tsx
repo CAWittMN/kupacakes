9b1ee147c844e4fca4960d207a4107bf0b8e7654
Casey Wittrock <caseywittrockmn@gmail.com>
2024-07-19 02:44:34 -0500

    create handle functions in app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,143 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./App.css";
+import { AppContext } from "./context/AppContext";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { RegisterData, NewCupcakeData } from "./types";
+
+import useLocalStorage from "./hooks/useLocalStorage";
+
+import KupacakesAPI from "./kupacakesAPI";
+
+const App = () => {
+  const [token, setToken] = useLocalStorage("token");
+  const [userId, setUserId] = useLocalStorage("userId");
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [errors, setErrors] = useState<string[]>([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      setIsLoading(true);
+      try {
+        if (token) {
+          KupacakesAPI.loadApi(token as string);
+        }
+      } catch (errors) {
+        setErrors(errors as string[]);
+      }
+      setIsLoading(false);
+    };
+    checkToken();
+  }, [token]);
+
+  const handleLogin = async (data: { username: string; password: string }) => {
+    setIsLoading(true);
+    try {
+      const { token, userId } = await KupacakesAPI.login(data);
+      setToken(token);
+      setUserId(userId);
+    } catch (errors) {
+      setErrors(errors as string[]);
+    }
+    setIsLoading(false);
+  };
+
+  const handleLogout = async () => {
+    setIsLoading(true);
+    try {
+      await KupacakesAPI.logout();
+      setUserId(null);
+      setToken(null);
+    } catch (errors) {
+      setErrors(errors as string[]);
+    }
+    setIsLoading(false);
+  };
+
+  const handleRegister = async (data: RegisterData) => {
+    setIsLoading(true);
+    try {
+      const { token, userId } = await KupacakesAPI.register(data);
+      setToken(token);
+      setUserId(userId);
+    } catch (errors) {
+      setErrors(errors as string[]);
+    }
+    setIsLoading(false);
+  };
+
+  const handleCreateCupcake = async (data: NewCupcakeData) => {
+    setIsLoading(true);
+    try {
+      const cupcake = await KupacakesAPI.generateCupcake(data);
+      setIsLoading(false);
+      navigate(`/cupcakes/${cupcake.id}`);
+    } catch (errors) {
+      setErrors(errors as string[]);
+      setIsLoading(false);
+    }
+  };
+
+  const handleGetCupcake = async (id: number) => {
+    setIsLoading(true);
+    try {
+      const cupcake = await KupacakesAPI.getCupcake(id);
+      setIsLoading(false);
+      return cupcake;
+    } catch (errors) {
+      setErrors(errors as string[]);
+      setIsLoading(false);
+    }
+  };
+
+  const handleGetCupcakes = async (userId: number | null = null) => {
+    setIsLoading(true);
+    try {
+      const cupcakes = await KupacakesAPI.getCupcakes(userId);
+      setIsLoading(false);
+      return cupcakes;
+    } catch (errors) {
+      setErrors(errors as string[]);
+      setIsLoading(false);
+    }
+  };
+
+  const handleDeleteCupcake = async (id: number) => {
+    setIsLoading(true);
+    try {
+      await KupacakesAPI.deleteCupcake(id);
+      setIsLoading(false);
+      navigate("/cupcakes");
+    } catch (errors) {
+      setErrors(errors as string[]);
+      setIsLoading(false);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <AppContext.Provider
+      value={{
+        userId,
+        isLoading,
+        errors,
+        handleLogin,
+        handleLogout,
+        handleRegister,
+        handleCreateCupcake,
+        handleGetCupcake,
+        handleGetCupcakes,
+        handleDeleteCupcake,
+      }}
+    >
+      <div className="App">
+        <header className="App-header">
+          <h1>KupaCakes</h1>
+        </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </AppContext.Provider>
+  );
+};
 
-export default App
+export default App;
